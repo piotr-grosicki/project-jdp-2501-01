8d208031176725ae298916a0b67869a640aa5510
Robert Wyszynski <robertointerwento@gmail.com>
2025-02-11 20:33:44 +0100

    JDP250101-3

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -15,12 +15,12 @@ public class CartController {
         return new ArrayList<>();
     }
 
-    @GetMapping(value = "{cartId}")
+    @GetMapping(value = "/{cartId}")
     public String getCart(@PathVariable Long CartId) {
         return "wartosc";
     }
 
-    @PutMapping
+    @PutMapping(value = "/{cartId}")
     public String updateCart(@RequestBody String cartDto) {
         return "wartosc";
     }
@@ -29,7 +29,7 @@ public class CartController {
     public void postCart(@RequestBody String cartDto) {
     }
 
-    @DeleteMapping
+    @DeleteMapping(value = "/{cartId}")
     public void deleteCart(@PathVariable Long CartId){
     }
 }
