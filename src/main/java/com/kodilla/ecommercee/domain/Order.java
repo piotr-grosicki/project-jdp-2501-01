6a2dea0602dbacde108aa6b5e3267755356dce3a
Katarzyna-Kantarowska <kantarowskakasia@gmail.com>
2025-02-16 19:44:21 +0100

    JDP250101-11 Entity implementation

---
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -0,0 +1,33 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long cartId;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private Date createdAt;
+}
