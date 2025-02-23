2bf53456f9963ce1dc7b4175ba6bf48d7a2b556a
Jareczeq <contact@jareczeq.pl>
2025-02-23 14:12:12 +0100

    Dodano sprawdzanie co się dzieje z encjami powiązanymi z encją Group

--- src/test/java/com/kodilla/ecommercee/GroupTests.java
+++ src/test/java/com/kodilla/ecommercee/GroupTests.java
@@ -1,11 +1,14 @@
 package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.domain.Group;
+import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.repository.GroupRepository;
+import com.kodilla.ecommercee.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +18,9 @@ public class GroupTests {
 
     @Autowired
     private GroupRepository groupRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @Test
     void testCreateGroup() {
@@ -67,6 +73,17 @@ public class GroupTests {
         Group group = new Group();
         group.setName("Group 1");
 
+        Product product = new Product();
+        product.setName("Product 1");
+        product.setDescription("Description 1");
+        product.setPrice(100.0);
+        product.setStockQuantity(15);
+
+        group = groupRepository.save(group);
+        product.setGroup(group);
+        product = productRepository.save(product);
+        group.setProducts(List.of(product));
+
         //When
         groupRepository.save(group);
         long groupId = group.getId();
@@ -74,6 +91,7 @@ public class GroupTests {
 
         //Then
         assertFalse(groupRepository.findById(groupId).isPresent());
+        assertFalse(productRepository.findById(product.getId()).isPresent());
     }
 
 }
