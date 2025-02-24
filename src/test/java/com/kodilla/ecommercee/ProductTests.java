65b4818e451a81e1fb465b479bbf775cbad5dffa
Katarzyna-Kantarowska <kantarowskakasia@gmail.com>
2025-02-24 12:40:57 +0100

    JDP250101-17 Tests for Product

--- src/test/java/com/kodilla/ecommercee/ProductTests.java
+++ src/test/java/com/kodilla/ecommercee/ProductTests.java
@@ -1,104 +1,136 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.repository.GroupRepository;
 import com.kodilla.ecommercee.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 @Transactional
 @SpringBootTest
-@AutoConfigureMockMvc
 public class ProductTests {
 
-    @MockBean private ProductRepository productRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Test
+    void shouldCreateProduct() {
+        //Given
+
+        Group group = Group.builder()
+                .name("group")
+                .products(new ArrayList<>())
+                .build();
+
+        Group savedGroup = groupRepository.save(group);
+
+        Product product = Product.builder()
+                .name("Tv")
+                .price(6200.0)
+                .description("Smart Tv")
+                .stockQuantity(10)
+                .group(savedGroup)
+                .build();
+
+        //When
+        Product savedProduct = productRepository.save(product);
+
+        //Then
+        assertNotNull(product.getId());
+        assertEquals("Tv", savedProduct.getName());
+    }
+
+    @Test
+    void shouldFindProduct() {
+        //Given
+        Group group = Group.builder()
+                .name("group")
+                .products(new ArrayList<>())
+                .build();
+
+        Group savedGroup = groupRepository.save(group);
+
+        Product product = Product.builder()
+                .name("Smartphone")
+                .price(1200.0)
+                .description("New Smartphone")
+                .stockQuantity(10)
+                .group(savedGroup)
+                .build();
+        Product savedProduct = productRepository.save(product);
+
+        //When
+        Optional<Product> foundProduct = productRepository.findById(savedProduct.getId());
+
+        //Then
+        assertTrue(foundProduct.isPresent());
+        assertEquals("Smartphone", foundProduct.get().getName());
+    }
 
 
+    @Test
+    void shouldUpdateProduct() {
+        //Given
+        Group group = Group.builder()
+                .name("group")
+                .products(new ArrayList<>())
+                .build();
 
-@Test
-void shouldCreateProduct() {
-   //Given
-    Product product = Product.builder()
-            .name("Tv")
-            .price(6200.0)
-            .description("Smart Tv")
-            .stockQuantity(10)
-            .build();
+        Group savedGroup = groupRepository.save(group);
 
-    when(productRepository.save(any())).thenReturn(product);
+        Product product = Product.builder()
+                .name("PC")
+                .price(3200.0)
+                .description("Best PC")
+                .stockQuantity(10)
+                .group(savedGroup)
+                .build();
 
-    //When
-    Product savedProduct = productRepository.save(product);
+        //When
+        product = productRepository.save(product);
+        product.setPrice(3099.9);
+        Product updatedProduct = productRepository.save(product);
 
-    //Then
-    assertEquals("Tv", savedProduct.getName());
-    verify(productRepository, times(1)).save(any());
-}
-@Test
-void shouldFindProduct() {
-    //Given
-    Product product = Product.builder()
-            .name("Smartphone")
-            .price(1200.0)
-            .description("New Smartphone")
-            .stockQuantity(10)
-            .build();
-   when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        //Then
+        assertEquals(3099.9, updatedProduct.getPrice());
+    }
 
-    //When
-    Optional<Product> foundProduct = productRepository.findById(1L);
-
-    //Then
-    assertTrue(foundProduct.isPresent());
-    assertEquals("Smartphone", foundProduct.get().getName());
-}
-
-
-@Test
-void shouldUpdateProduct() {
-    //Given
-    Product product = Product.builder()
-            .name("PC")
-            .price(3200.0)
-            .description("Best PC")
-            .stockQuantity(10)
-            .build();
-    when(productRepository.save(any())).thenReturn(product);
-
-    //When
-    product = productRepository.save(product);
-    product.setPrice(3099.9);
-    Product updatedProduct = productRepository.save(product);
-
-    //Then
-    assertEquals(3099.9, updatedProduct.getPrice());
-}
-
-@Test
+    @Test
     void shouldDeleteProduct() {
-    //Given
-    Product product = Product.builder()
-            .id(2L)
-            .name("Mouse")
-            .price(50.0)
-            .description("Gaming mouse")
-            .stockQuantity(30)
-            .build();
+        //Given
+        Group group = Group.builder()
+                .name("group")
+                .products(new ArrayList<>())
+                .build();
 
-    when((productRepository.findById(2L))).thenReturn(Optional.of(product));
+        Group savedGroup = groupRepository.save(group);
 
-    //When
-    productRepository.deleteById(2L);
+        Product product = Product.builder()
+                .id(2L)
+                .name("Mouse")
+                .price(50.0)
+                .description("Gaming mouse")
+                .stockQuantity(30)
+                .group(savedGroup)
+                .build();
 
-    //Then
-    when(productRepository.findById(2L)).thenReturn(Optional.empty());
-    Optional<Product> deletedProduct = productRepository.findById(2L);
-    assertFalse(deletedProduct.isPresent());
+        product = productRepository.save(product);
+        Long productId = product.getId();
+        //When
+        productRepository.deleteById(productId);
+
+        //Then
+        Optional<Product> deletedProduct = productRepository.findById(productId);
+        assertFalse(deletedProduct.isPresent());
     }
 }
